e0eac79f87e8b7d4bf8e8ef92194f5446d667eb3
Albert Luis Pereira de Jesus <albertluis123y88@gmail.com>
2025-10-02 17:39:33 -0300

    banco

--- back/src/main/java/com/example/locadora/configs/SecurityConfig.java
+++ back/src/main/java/com/example/locadora/configs/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.example.locadora.configs;
 
-import com.example.locadora.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,7 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import com.example.locadora.security.JwtAuthFilter;
 
 @Configuration
 @EnableWebSecurity
